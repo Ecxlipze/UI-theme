fad6dbe782128bcff2e808f9a2c80fab1dcc5c0e
Ecxlipze <102612004+Ecxlipze@users.noreply.github.com>
2025-09-22 16:56:14 +0500

    Refactor JS and CSS, update title in index.html
    Removed unused CSS rules and non-standard 'text-stroke' properties for cleaner styles. Improved JavaScript by removing redundant comments, adding plugin existence checks, updating carousel breakpoints, and refactoring reservation form submission for better error handling and user feedback. Updated the HTML title to 'Luxex'.

--- js/luxex.js
+++ js/luxex.js
@@ -1,43 +1,11 @@
-/*
-  [JS Index]
-*/
-
-
-/*
-  1. preloader
-  2. fadeIn.element
-  3. to top animation
-  4. page scroll
-  5. navigation
-  6. navigation dropdown
-  7. animate elements
-  8. facts counter 
-  9. google maps zoom ON/OFF
-  10. owl carousel
-  11. swiper slider
-  12. magnificPopup
-  13. clone function
-  14. items selector
-  15. datepicker
-  16. Select2
-  17. toggle blog panels
-  18. contact form
-    18.1. contact modal
-  19. reservation form
-  20. blog comment form
-*/
-
-
 $(function() {
     "use strict";
-	
-	
+    
+    
     $(window).on("load", function() {
-        // 1. preloader
         $("#preloader").fadeOut(600);
         $(".preloader-bg").delay(400).fadeOut(600);
-		
-        // 2. fadeIn.element
+        
         setTimeout(function() {
             $(".fadeIn-element").delay(600).css({
                 display: "none"
@@ -45,26 +13,24 @@ $(function() {
         }, 0);
         $(".hero-bg").addClass("hero-bg-show");
     });
-	
-    // 3. to top animation
+    
     $(".to-top-arrow").on("click", function() {
         $("body, html").animate({
             scrollTop: 0
         }, 1000);
         return false
     })
-	
-    // 4. page scroll													   
+    
     $('a[href*="#"]').not('[href="#"]').not('[href="#collapseOne"]').not('[href="#collapseTwo"]').not('[href="#collapseThree"]').not('[href="#collapseFour"]').not('[href="#collapseFive"]').not(
         '[href="#collapseSix"]').on("click", function() {
         console.log("click");
         if (location.pathname.replace(/^\//, "") == this.pathname.replace(/^\//, "") && location.hostname == this.hostname) {
             var target = $(this.hash);
-            target = target.length ? target : $('[name=" + this.hash.slice(1) + "]');
+            target = target.length ? target : $('[name="' + this.hash.slice(1) + '"]');
             if (target.length) {
                 if ($(window).width() < 768) {
                     $("html, body").animate({
-                        scrollTop: target.offset().top - 80
+                        scrollTop: target.offset().top - 50
                     }, 1000);
                 } else {
                     $("html, body").animate({
@@ -75,13 +41,11 @@ $(function() {
             }
         }
     });
-	
-    // 5. navigation
+    
     $(".navbar-collapse ul li a.nav-close").on("click", function() {
         $(".navbar-toggle:visible").click();
     });
-	
-    // 6. navigation dropdown
+    
     $(".dropdown-toggle").on("click", function(e) {
         if ($(document).width() > 768) {
             e.preventDefault();
@@ -91,9 +55,8 @@ $(function() {
             }
         }
     });
-	
+    
     $(window).on("scroll", function() {
-        // 7. animate elements
         if ($(this).scrollTop() > 50) {
             $(".border-top").addClass("top-position-primary");
             $(".navbar-bg-switch").addClass("navbar-bg-switch-color");
@@ -102,7 +65,7 @@ $(function() {
             $(".main-navigation").addClass("main-navigation-home-call");
             $(".navbar-bg-switch").addClass("main-navigation-bg");
             $(".to-top-arrow").addClass("show");
-			$(".blog-side-launcher").addClass("blog-side-launcher-color");
+            $(".blog-side-launcher").addClass("blog-side-launcher-color");
         } else {
             $(".border-top").removeClass("top-position-primary");
             $(".navbar-bg-switch").removeClass("navbar-bg-switch-color");
@@ -111,11 +74,11 @@ $(function() {
             $(".main-navigation").removeClass("main-navigation-home-call");
             $(".navbar-bg-switch").removeClass("main-navigation-bg");
             $(".to-top-arrow").removeClass("show");
-			$(".blog-side-launcher").removeClass("blog-side-launcher-color");
+            $(".blog-side-launcher").removeClass("blog-side-launcher-color");
         }
     });
-	
-    // 8. facts counter
+    
+    if (typeof $.fn.appear !== 'undefined' && typeof $.fn.countTo !== 'undefined') {
     $(".facts-counter-number").appear(function() {
         var count = $(this);
         count.countTo({
@@ -125,16 +88,18 @@ $(function() {
             refreshInterval: 60
         });
     });
-	
-    // 9. google maps zoom ON/OFF
+    } else {
+    console.log("Appear.js or CountTo.js not loaded - facts counter disabled");
+    }
+    
     $(".google-maps").on("click", function() {
         $('.google-maps iframe').css("pointer-events", "auto");
     });
     $(".google-maps").on("mouseleave", function() {
         $('.google-maps iframe').css("pointer-events", "none");
     });
-	
-    // 10. owl carousel
+    
+   if (typeof $.fn.owlCarousel !== 'undefined') {
     $(".owl-carousel-testimonials").owlCarousel({
         loop: true,
         center: true,
@@ -148,6 +113,7 @@ $(function() {
         animateOut: "fadeOut",
         animateIn: "fadeIn"
     });
+    
     $(".owl-carousel-rooms").owlCarousel({
         loop: false,
         center: false,
@@ -155,29 +121,33 @@ $(function() {
         autoplaySpeed: 1000,
         autoplayTimeout: 5000,
         smartSpeed: 450,
-        nav: false,
         nav: true,
         navText: ["<i class='ion-chevron-left'></i>", "<i class='ion-chevron-right'></i>"],
         navContainer: '.owl-nav-custom-rooms',
         responsive: {
             0: {
                 items: 1,
-                margin: 20
+                margin: 10
+            },
+            480: {
+                items: 1,
+                margin: 15
             },
             768: {
-                items: 1,
+                items: 2,
                 margin: 20
             },
             980: {
                 items: 2,
-                margin: 50
+                margin: 30
             },
             1240: {
                 items: 3,
-                margin: 50
+                margin: 40
             }
         }
     });
+    
     $(".owl-carousel-facilities").owlCarousel({
         loop: false,
         center: false,
@@ -185,29 +155,33 @@ $(function() {
         autoplaySpeed: 1000,
         autoplayTimeout: 5000,
         smartSpeed: 450,
-        nav: false,
         nav: true,
         navText: ["<i class='ion-chevron-left'></i>", "<i class='ion-chevron-right'></i>"],
         navContainer: '.owl-nav-custom-facilities',
         responsive: {
             0: {
                 items: 1,
-                margin: 20
+                margin: 10
+            },
+            480: {
+                items: 1,
+                margin: 15
             },
             768: {
-                items: 1,
+                items: 2,
                 margin: 20
             },
             980: {
                 items: 2,
-                margin: 50
+                margin: 30
             },
             1240: {
                 items: 3,
-                margin: 50
+                margin: 40
             }
         }
     });
+    
     $(".owl-carousel-team").owlCarousel({
         loop: false,
         center: false,
@@ -215,29 +189,33 @@ $(function() {
         autoplaySpeed: 1000,
         autoplayTimeout: 5000,
         smartSpeed: 450,
-        nav: false,
         nav: true,
         navText: ["<i class='ion-chevron-left'></i>", "<i class='ion-chevron-right'></i>"],
         navContainer: '.owl-nav-custom-team',
         responsive: {
             0: {
                 items: 1,
+                margin: 10
+            },
+            480: {
+                items: 2,
                 margin: 20
             },
             768: {
                 items: 2,
-                margin: 50
+                margin: 30
             },
             880: {
                 items: 2,
-                margin: 50
+                margin: 30
             },
             1240: {
                 items: 3,
-                margin: 50
+                margin: 40
             }
         }
     });
+    
     $("#owl-carousel-all").owlCarousel({
         loop: false,
         center: false,
@@ -245,29 +223,33 @@ $(function() {
         autoplaySpeed: 1000,
         autoplayTimeout: 5000,
         smartSpeed: 450,
-        nav: false,
         nav: true,
         navText: ["<i class='ion-chevron-left'></i>", "<i class='ion-chevron-right'></i>"],
         navContainer: '.owl-nav-custom-all',
         responsive: {
             0: {
                 items: 1,
-                margin: 20
+                margin: 10
+            },
+            480: {
+                items: 1,
+                margin: 15
             },
             768: {
-                items: 1,
+                items: 2,
                 margin: 20
             },
             980: {
                 items: 2,
-                margin: 50
+                margin: 30
             },
             1240: {
                 items: 2,
-                margin: 50
+                margin: 40
             }
         }
     });
+    
     $(".owl-carousel-menu").owlCarousel({
         loop: false,
         center: false,
@@ -277,17 +259,20 @@ $(function() {
         autoplaySpeed: 1000,
         autoplayTimeout: 5000,
         smartSpeed: 450,
-        nav: false,
         nav: true,
         navText: ["<i class='ion-chevron-left'></i>", "<i class='ion-chevron-right'></i>"],
         navContainer: '.owl-nav-custom-menu'
     });
-	
-    // 11. swiper slider
+    
+} else {
+    console.log("Owl Carousel not loaded - carousels will not function");
+    $('.owl-carousel').css('overflow-x', 'auto').css('white-space', 'nowrap');
+    $('.owl-carousel > div').css('display', 'inline-block');
+}
+    
     var swiper1 = new Swiper(".swiper-container-wrapper .swiper-container.swiper1", {
         preloadImages: false,
-        // autoplay: false,
-		autoplay: {
+        autoplay: {
             delay: 4000,
             disableOnInteraction: false
         },
@@ -307,7 +292,7 @@ $(function() {
         },
         scrollbar: false
     });
-	var swiper2 = new Swiper(".swiper-container-wrapper .swiper-container.swiper2", {
+    var swiper2 = new Swiper(".swiper-container-wrapper .swiper-container.swiper2", {
         preloadImages: false,
         autoplay: false,
         init: true,
@@ -320,7 +305,7 @@ $(function() {
         pagination: false,
         navigation: false
     });
-	var swiper3 = new Swiper(".swiper-container-wrapper .swiper-container.swiper3", {
+    var swiper3 = new Swiper(".swiper-container-wrapper .swiper-container.swiper3", {
         preloadImages: false,
         autoplay: false,
         init: true,
@@ -337,12 +322,12 @@ $(function() {
             nextEl: ".slide-next",
             prevEl: ".slide-prev"
         },
-		scrollbar: false,
-		slidesPerView: 2,
+        scrollbar: false,
+        slidesPerView: 2,
         slidesPerColumn: 2
     });
-	
-    // 12. magnificPopup
+    
+    if (typeof $.fn.magnificPopup !== 'undefined') {
     $(".popup-photo").magnificPopup({
         type: "image",
         gallery: {
@@ -367,8 +352,15 @@ $(function() {
             fixedContentPos: false
         });
     });
-	
-    // 13. clone function
+    
+} else {
+    console.log("Magnific Popup not loaded - image lightboxes disabled");
+    $('.popup-photo').on('click', function(e) {
+        e.preventDefault();
+        window.open($(this).attr('href'), '_blank');
+    });
+}
+    
     $.fn.duplicate = function(count, cloneEvents, callback) {
         var stack = [],
             el;
@@ -381,8 +373,7 @@ $(function() {
     };
     $("<div class='vertical-lines-wrapper'></div>").appendTo(".vertical-lines");
     $("<div class='vertical-effect'></div>").duplicate(3).appendTo(".vertical-lines-wrapper");
-	
-    // 14. items selector
+    
     const buttons = document.querySelectorAll(".item-button");
     const menus = document.querySelectorAll(".menu");
     function handleClick(e) {
@@ -394,18 +385,23 @@ $(function() {
         });
     }
     buttons.forEach(button => button.addEventListener("click", handleClick));
-	
-    // 15. datepicker
+    
+    if (typeof $.fn.datepicker !== 'undefined') {
     $(".datepicker").datepicker({
         orientation: "auto"
     });
-	
-    // 16. Select2
+    } else {
+    console.log("jQuery UI Datepicker not loaded - date inputs will be basic text fields");
+}
+    
+    if (typeof $.fn.select2 !== 'undefined') {
     $(".select2").select2({
         minimumResultsForSearch: Infinity
     });
-	
-    // 17. toggle blog panels
+    } else {
+    console.log("Select2 not loaded - dropdowns will be basic select elements");
+}
+    
     $(".blog-side-launcher").on("click", function() {
         var divClass = $(this).attr("data-id");
         if ($(this).hasClass("open")) {
@@ -419,8 +415,7 @@ $(function() {
     $(".blog-side-launch, .blog-side-text").on("click", function() {
         $(".panel-from-left-blog, .panel-overlay-from-right-blog").removeClass("open");
     });
-	
-    // 18. contact form
+    
     $("form#form").on("submit", function() {
         $("form#form .error").remove();
         var s = !1;
@@ -445,7 +440,6 @@ $(function() {
         }
         return !1;
     });
-    // 18.1. contact modal
     $(".contact-modal-launcher, .contact-modal-closer").on("click", function(e) {
         e.preventDefault();
         if ($(".contact-modal").hasClass("open")) {
@@ -454,34 +448,50 @@ $(function() {
             $(".contact-modal").addClass("open");
         }
     });
-	
-    // 19. reservation form
-    $("form#form-2").on("submit", function() {
-        $("form#form-2 .error-r").remove();
-        var s = !1;
-        if ($(".requiredField-r").each(function() {
-                if ("" === jQuery.trim($(this).val())) $(this).prev("label").text(), $(this).parent().append('<span class="error-r">This field is required</span>'), $(this).addClass(
-                    "inputError"), s = !0;
-                else if ($(this).hasClass("email")) {
-                    var r = /^([\w-\.]+@([\w-]+\.)+[\w-]{2,4})?$/;
-                    r.test(jQuery.trim($(this).val())) || ($(this).prev("label").text(), $(this).parent().append('<span class="error-r">Invalid email address</span>'), $(this).addClass(
-                        "inputError"), s = !0);
-                }
-            }), !s) {
-            $("form#form-2 input.submit").fadeOut("normal", function() {
-                $(this).parent().append("");
-            });
-            var r = $(this).serialize();
-            $.post($(this).attr("action"), r, function() {
-                $("form#form-2").slideUp("fast", function() {
-                    $(this).before('<div class="success-r">Your reservation has been received.</div>');
-                });
-            });
+    
+    $("form#form-2").on("submit", function(e) {
+    e.preventDefault();
+    $("form#form-2 .error-r").remove();
+    var hasErrors = false;
+    
+    $(".requiredField-r").each(function() {
+        if ($(this).val().trim() === "") {
+            $(this).parent().append('<span class="error-r">This field is required</span>');
+            $(this).addClass("inputError");
+            hasErrors = true;
+        } else if ($(this).hasClass("email")) {
+            var emailRegex = /^([\w-\.]+@([\w-]+\.)+[\w-]{2,4})?$/;
+            if (!emailRegex.test($(this).val().trim())) {
+                $(this).parent().append('<span class="error-r">Invalid email address</span>');
+                $(this).addClass("inputError");
+                hasErrors = true;
+            }
         }
-        return !1;
     });
-	
-    // 20. blog comment form
+    
+    if (!hasErrors) {
+        var formData = $(this).serialize();
+        var submitBtn = $(this).find('input[type="submit"], button[type="submit"]');
+        submitBtn.prop('disabled', true).val('Sending...');
+        
+        $.ajax({
+            url: $(this).attr("action") || "reservation.php",
+            type: "POST",
+            data: formData,
+            success: function(response) {
+                $(this).slideUp("fast", function() {
+                    $(this).before('<div class="success-r">Your reservation has been received. We will contact you soon!</div>');
+                });
+            }.bind(this),
+            error: function(xhr, status, error) {
+                submitBtn.prop('disabled', false).val('Book now');
+                $(this).prepend('<div class="error-r" style="color: red; margin-bottom: 10px;">Error sending reservation. Please try again or contact us directly.</div>');
+            }.bind(this)
+        });
+    }
+    return false;
+});
+    
     $("form#form-3").on("submit", function() {
         $("form#form-3 .error-c").remove();
         var s = !1;
@@ -506,6 +516,6 @@ $(function() {
         }
         return !1;
     });
-	
-	
+    
+    
 });
